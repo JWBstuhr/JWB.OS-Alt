d2593b867344a5f15375f0a3421cc5456ce6e40d
JWBstuhr <77174240+JWBstuhr@users.noreply.github.com>
2021-01-25 10:50:49 -0600

    Delete os directory

--- os/chath.lua
+++
@@ -1,8 +0,0 @@
-term.clear()
-term.setCursorPos(1,1)
-print("What channel do you want to host on?")
-print("(Input a Number)")
-input = read()
-local chost = input
-shell.run("fg chat host "..chost)
-shell.run("back")
