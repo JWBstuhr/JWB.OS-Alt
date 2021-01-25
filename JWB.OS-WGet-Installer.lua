fe0a5e2daaa337ed70d2ac6a4c315b2aff1f2b7e
JWBstuhr <77174240+JWBstuhr@users.noreply.github.com>
2021-01-25 11:33:26 -0600

    Update JWB.OS-WGet-Installer.lua

--- JWB.OS-WGet-Installer.lua
+++ JWB.OS-WGet-Installer.lua
@@ -1,4 +1,4 @@
---[ Run 'wget run https://github.com/JWBstuhr/JWB.OS/JWB.OS-WGet-Installer.lua' to Install ]--
+--[ Run 'wget run https://github.com/JWBstuhr/JWB.OS/blob/main/JWB.OS-WGet-Installer.lua' to Install ]--
 
 print("Installing...")
 shell.run("wget https://github.com/JWBstuhr/JWB.OS/blob/main/startup.lua startup.lua")
