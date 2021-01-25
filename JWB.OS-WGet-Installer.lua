f9d1b0eb642d4abcf2f2358084d57e3b317b55be
JWBstuhr <77174240+JWBstuhr@users.noreply.github.com>
2021-01-25 11:39:23 -0600

    Update JWB.OS-WGet-Installer.lua

--- JWB.OS-WGet-Installer.lua
+++ JWB.OS-WGet-Installer.lua
@@ -1,14 +1,14 @@
---[ Run 'wget https://github.com/JWBstuhr/JWB.OS/blob/main/JWB.OS-WGet-Installer.lua jwbos-installer.lua' to get Installer ]--
+--[ Run 'wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/JWB.OS-WGet-Installer.lua' to get Installer ]--
 
 print("Installing...")
-shell.run("wget https://github.com/JWBstuhr/JWB.OS/blob/main/startup.lua startup.lua")
-shell.run("wget https://github.com/JWBstuhr/JWB.OS/blob/main/.back.lua .back")
+shell.run("wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/startup.lua startup.lua")
+shell.run("wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/.back.lua .back")
 shell.run("mkdir os")
-shell.run("wget https://github.com/JWBstuhr/JWB.OS/blob/main/os/.files.lua os/.files")
-shell.run("wget https://github.com/JWBstuhr/JWB.OS/blob/main/os/.cmd.lua os/.cmd")
-shell.run("wget https://github.com/JWBstuhr/JWB.OS/blob/main/os/.programs.lua os/.programs")
-shell.run("wget https://github.com/JWBstuhr/JWB.OS/blob/main/os/.chath.lua os/.chath")
-shell.run("wget https://github.com/JWBstuhr/JWB.OS/blob/main/os/.chatj.lua os/.chatj")
-shell.run("wget https://github.com/JWBstuhr/JWB.OS/blob/main/os/.uninstall.lua os/.uninstall")
+shell.run("wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/os/.files.lua os/.files")
+shell.run("wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/os/.cmd.lua os/.cmd")
+shell.run("wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/os/.programs.lua os/.programs")
+shell.run("wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/os/.chath.lua os/.chath")
+shell.run("wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/os/.chatj.lua os/.chatj")
+shell.run("wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/os/.uninstall.lua os/.uninstall")
 print("Done. Rebooting...")
 sleep(2)
